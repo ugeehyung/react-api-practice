0e7114dd81086190980318ee77af25bfead04e88
Jihyung <jihyung910@gmail.com>
2021-07-16 14:16:03 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div> HELLO :) </div>
-  );
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    music: []
+  };
+  
+  componentDidMount() {
+    //음악 데이터 로딩
+    axios.get('https://ws.audioscrobbler.com/2.0/?method=track.search&track=BTS&api_key=cc0c40005535fde9039f0a38b2631bf0&format=json');
+  }
+
+  render() {  
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
+  }
 }
 
 export default App;
