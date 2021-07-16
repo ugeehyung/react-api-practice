87eb25d7536c95ec2b4973b4e595a14d35b0d151
Jihyung <jihyung910@gmail.com>
2021-07-16 14:38:59 +0900

    c

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
   };
   
   componentDidMount() {
+    
     //음악 데이터 로딩
     axios.get('https://ws.audioscrobbler.com/2.0/?method=track.search&track=BTS&api_key=cc0c40005535fde9039f0a38b2631bf0&format=json');
   }
