fbcac12d945892101a9de8e048ada4e23b94bb57
Jihyung <jihyung910@gmail.com>
2021-07-18 14:12:03 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
 
@@ -13,8 +13,8 @@ class App extends React.Component {
       data : {
         data : { music },
       },
-    } = await axios.get("https://ws.audioscrobbler.com/2.0/?method=track.search&track=&api_key=cc0c40005535fde9039f0a38b2631bf0&format=json");
-    this.setState({ music });
+    } = await axios.get("https://ws.audioscrobbler.com/2.0/?method=track.search&track=bts&api_key=cc0c40005535fde9039f0a38b2631bf0&format=json");
+    this.setState({ music, isLoading: false });
   }
 
   componentDidMount() {
