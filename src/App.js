860fe98c75539057c35f2a41a730da1a0a3366c0
Jihyung <jihyung910@gmail.com>
2021-07-25 18:07:46 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import axios from 'axios';
+import Home from './routes/Home';
+//import './App.css';
 
-class App extends React.Component {
+function App() {
 
-  state = {
-    isLoading: true,
-    music: [],
-  };
-  
-  getMusic = async () => {
-    const {
-      data : {
-        data : { music },
-      },
-    } = await axios.get("https://ws.audioscrobbler.com/2.0/?method=track.search&track=bts&api_key=cc0c40005535fde9039f0a38b2631bf0&format=json");
-    this.setState({ music, isLoading: false });
-  }
+  return <Home />;
 
-  componentDidMount() {
-    this.getMusic();
-  }
-
-  render() {  
-    const { isLoading } = this.state;
-    return <div>{ isLoading ? 'Loading...' : 'Start...' }</div>;
-  }
 }
 
 export default App;
